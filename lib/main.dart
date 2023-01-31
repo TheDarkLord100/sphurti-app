735f3c1a6960831a6b83e7d404d79e36a7a8362c
Mudit jain <muditj214@gmail.com>
2023-01-31 23:49:03 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:sphurti_app/screens/navigation_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -17,6 +17,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: NavBarScreen(),
+    );
   }
 }
